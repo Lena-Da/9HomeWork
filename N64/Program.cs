a6326b6b8687a18331421537e1e6971a2f4bb4ae
Lena <len.dal@yandex.ru>
2022-07-09 16:10:25 +0300

    first task

---
+++ N64/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1 (Рекурсия)
+
+int m = InputInt("Задайте число m: ");
+int n = 1;
+Console.WriteLine(NaturalNumber(m, n));
+
+int NaturalNumber(int m, int n)
+{
+if (m == n)
+{
+return n;
+}
+if (m < n)
+{
+Console.Write($"{NaturalNumber(m, n - 1)}, ");
+}
+if (m > n)
+{
+Console.Write($"{NaturalNumber(m, n + 1)}, ");
+}
+
+return n;
+}
+
+int InputInt(string output)
+{
+Console.Write(output);
+return int.Parse(Console.ReadLine());
+}
+
