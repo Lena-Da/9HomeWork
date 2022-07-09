5f1ee6954ce94edb75be1b7cab398da4b6bf9469
Lena <len.dal@yandex.ru>
2022-07-09 18:41:14 +0300

    third task

---
+++ N68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+int m = InputInt("Задайте число m: ");
+int n = InputInt("Задайте число n: ");
+
+int AkkermanFunction = Akkerman(m, n);
+
+Console.Write($"Функция Аккермана = {AkkermanFunction} ");
+
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int InputInt(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
